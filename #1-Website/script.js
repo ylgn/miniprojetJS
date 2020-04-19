88605d2b4f700868a444d5ef4c545de8765fb5ad
ylgn <yannis.belkebir@etu.parisdescartes.fr>
2020-04-19 22:07:39 +0200

    End of project

--- #1-Website/script.js
+++ #1-Website/script.js
@@ -5,6 +5,7 @@ function Product(idProduct, decription, quantity, price, region) {
     this.quantity = quantity;
     this.price = price;
     this.region = region;
+    this.picture = "content/cow.jpg";
     Product.prototype.getTypeProduct = function () {return this.typeProduct;} 
     Product.prototype.getID = function () { return this.idProduct; }
     Product.prototype.setID = function (id) { this.idProduct = id; }
@@ -16,6 +17,8 @@ function Product(idProduct, decription, quantity, price, region) {
     Product.prototype.setPrice = function (price) { this.price = price; }
     Product.prototype.getRegion = function () { return this.region; }
     Product.prototype.setRegion = function (r) { this.region = r; }
+    Product.prototype.getPicture = function () { return this.picture; }
+    Product.prototype.setPicture = function (path) { this.picture = path;}
     Product.prototype.toString = function () {
         return "Product n° " + this.idProduct + ", Nom : " + this.decription + ", Quantité en stock : " + this.quantity + ", Prix par unité : " + this.price + ", Origine :" + this.region;
 }
@@ -44,7 +47,7 @@ DiverProduct.prototype = Object.create(Product.prototype);
 DiverProduct.prototype.constructor = DiverProduct;
 
 
-test = new Product("00", "Test", 110, 2, "Poissy");
+test = new Product("00", "Test Covid", 0, 10, "Poissy");
 bananas = new Fruit("01", "Banane", 110, 2, "La Réunion");
 watermelons = new Fruit("02","Melon",1000,3,"Maroc");
 milk = new DiverProduct("01","Lait",1000,1,"Allemagne");
@@ -53,11 +56,11 @@ beans = new Vegetable("01","Poids",4,12,"Finistère");
 mushromms = new Vegetable("02","Champignons",4,12,"Finistère");
 
 var Products =[test,bananas,watermelons,milk,honey,beans,mushromms];
-
+var numProd = 2;
   function getListByType(typeP){
     var typeArray = [];
     for (let index = 0; index < Products.length; index++) {
-        if (Products[index].getTypeProduct() == typeP) {
+        if (Products[index].getTypeProduct() == typeP && Products[index].getQuantity()>0) {
             typeArray.push(Products[index]);
         }
     }return typeArray; 
@@ -69,6 +72,7 @@ var Products =[test,bananas,watermelons,milk,honey,beans,mushromms];
     for (let index = 0; index < nbProducts; index++) {
         const p = typeArray[index];
         document.write("<tr>");
+        document.write('<td><img src="'+p.getPicture()+'"></td>');
         document.write("<td>"+p.getID()+"</td>");
         document.write("<td>"+p.getDescription()+"</td>");
         document.write("<td>"+p.getQuantity()+"</td>");
@@ -77,4 +81,77 @@ var Products =[test,bananas,watermelons,milk,honey,beans,mushromms];
         document.write("</tr>");
     }
   }
+
+  function addProduct(typeProduct,decription,quantity,price,region){
+        if (typeProduct == "fruit") {
+            Products.push(new Fruit(numProd++,decription,quantity,price,region));
+        }
+        if (typeProduct == "vegetable") {
+            Products.push(new Vegetable(numProd++,decription,quantity,price,region));
+        } else {
+            Products.push(new DiverProduct(numProd++,decription,quantity,price,region));
+        }
+  }
+
+    function deleteProduct(idProduct){
+  	for (let index = 0; index < Products.length; index++) {
+        if (Products[index].getID() == idProduct) {
+           Products[index].setQuantity(0);
+        }   
+    }
+  }
+
+   function modifyProduct(idProduct,decription,quantity,price,region){
+  	for (let index = 0; index < Products.length; index++) {
+        if (Products[index].getID() == idProduct) {
+           Products[index].setDescription(description)
+           Products[index].setQuantity(quantity)
+           Products[index].setPrice(price)
+           Products[index].setRegion(quantity)
+        }   
+    }
+  }
+
+   function displayTabByAll(){
+    Products
+    for (let index = 0; index < Products.length; index++) {
+        const p = Products[index];
+        document.write("<tr>");
+        document.write('<td><img src="'+p.getPicture()+'"></td>');
+        document.write("<td>"+p.getID()+"</td>");
+        document.write("<td>"+p.getDescription()+"</td>");
+        document.write("<td>"+p.getQuantity()+"</td>");
+        document.write("<td>"+p.getPrice()+"€</td>");
+        document.write("<td>"+p.getRegion()+"</td>");
+        document.write("</tr>");
+    }
+  }
+
+
+ function displayTabByQuantityZero(){
+    var typeArray = getListByStock();
+    let nbProducts = typeArray.length;
+    for (let index = 0; index < nbProducts; index++) {
+        const p = typeArray[index];
+        document.write("<tr>");
+        document.write('<td><img src="'+p.getPicture()+'"></td>');
+        document.write("<td>"+p.getID()+"</td>");
+        document.write("<td>"+p.getDescription()+"</td>");
+        document.write("<td>"+p.getQuantity()+"</td>");
+        document.write("<td>"+p.getPrice()+"€</td>");
+        document.write("<td>"+p.getRegion()+"</td>");
+        document.write("</tr>");
+    }
+  }
+
+  function getListByStock(){
+    var typeArray = [];
+    for (let index = 0; index < Products.length; index++) {
+        if (Products[index].getQuantity() <1) {
+            typeArray.push(Products[index]);
+        }
+    }return typeArray; 
+  }
+
+
 
